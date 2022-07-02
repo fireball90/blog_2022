e12fb22e819e4c2660300934d68fbb9a65b0af7f
fireball90 <58610433+fireball90@users.noreply.github.com>
2022-07-02 17:17:12 +0200

    menu javitas js szarakodas

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+var isLoggedIn=false;
+
 
 document.getElementById('login').onsubmit = function login(event){
     event.preventDefault();
@@ -7,4 +9,24 @@ document.getElementById('login').onsubmit = function login(event){
 
     console.log(name)
     console.log(pass)
+
+    if (name == "admin" && pass== "admin"){
+        isLoggedIn=true;
+    }else{
+        isLoggedIn=false;
+    }
+
+    if (isLoggedIn===true){
+        var belepes = document.getElementById('gombBe');
+        belepes.classList.add("hidden");
+        var register = document.getElementById('gombReg');
+        register.classList.add("hidden");
+    }
 }
+
+/* document.getElementById('gombKi').onclick = function logout(event){
+    event.preventDefault();
+    alert("Sikeres kilépés!");
+    location.href = "index.html";
+    isLoggedIn=false;
+} */
--- js/script.js
+++ js/script.js
@@ -112,42 +112,7 @@ fetch('https://localhost:7053/api/Blog/Read?page=1')
   .then(data => console.log(data))
   */
 
-var postHTML = '';
-var authorPls='Nincs név';
-fetch('https://localhost:7053/api/Blog/Read?page=1')
-  .then(function (response) {
-      return response.json();
-  })
-  .then(function (data) {
-      appendData(data);
-      console.log(response.json());
-  })
-  .catch(function (err) {
-      console.log('error: ' + err);
-  });
-function appendData(data) {
 
-  for (var i = 0; i < data.length; i++) {
-    postHTML+=`
-    <div class="card-container">
-     <div class="name-container"> 
-         <h3 class="title">${data[i].title}</h3> 
-         <hr>
-     </div> 
-     <div class="entry">
-        <p>${data[i].entry}</p>
-     </div>
-    <hr>
-     <p class="author">${data[i].authorNickname}</p>  
-    </div>
-    `
-
-/*       var div = document.createElement("div");
-      div.innerHTML = 'Name: ' + data[i].title + ' ' + data[i].entry;
-      mainContainer.appendChild(div); */
-  }
-  document.getElementById('list-container2').innerHTML = postHTML;
-}
 
 //logout
 document.getElementById('gombKi').onclick = function logout(event){
